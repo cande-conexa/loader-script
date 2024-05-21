4752d98447f61fc44a466c71dbcef7b9c0b4ff74
cande-conexa <candela@conexa.ai>
2024-05-21 18:39:54 -0300

    ready

--- src/index.js
+++ src/index.js
@@ -2,15 +2,72 @@
 const envLogin = 'process.env.LOGIN_URL';
 const envApp = 'process.env.APP_URL';
 
+const styles = `
+  :root {
+    --cyan-100: #09c;
+    --cyan-50: #b3e0ff;
+}
+.containerOverlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(255, 255, 255);
+  z-index: 9999;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.loaderContainer {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center !important;
+    align-content: center !important;
+    align-items: center !important;
+    align-self: center !important;
+}
+.root {
+    --size: 32px;
+    --maxBorder: 3px;
+    width: var(--size, 32px);
+    height: var(--size, 32px);
+    border-radius: 50%;
+    display: inline-block;
+    border-top: var(--maxBorder, 3px) solid var(--cyan-100, #09c);
+    border-right: var(--maxBorder, 3px) solid transparent;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite;
+    margin: 0 auto;
+    position: relative;
+}
+.root::after {
+    content: '';
+    box-sizing: border-box;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: var(--size, 32px);
+    height: var(--size, 32px);
+    border-radius: 50%;
+    border-left: var(--maxBorder, 3px) solid var(--cyan-50, #b3e0ff);
+    border-bottom: var(--maxBorder, 3px) solid transparent;
+    animation: rotation 0.5s linear infinite reverse;
+}
+@keyframes rotation {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+}
+  `;
 // cargar stylesheet
-const loadStyles = () => {
-  const link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.href = `${envApp}/src/styles.css`;
-  document.head.appendChild(link);
-};
-
-loadStyles();
+const style = document.createElement('style');
+style.innerHTML = styles;
+document.head.appendChild(style);
 
 // append loader to body overlay
 const container = document.createElement('div');
